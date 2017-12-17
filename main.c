417f371c275892d4cba925dae61e637087eb2290
sarazalabany <sara.zalabny@gmail.com>
2017-12-17 01:49:50 +0100

    new edit

--- main.c
+++ main.c
@@ -6,7 +6,17 @@
  */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+       initMSP ();
+
+       P3DIR |= BIT6 ; // P3 .6 output for PWM
+       P3SEL |= BIT6 ; // P3 .6 TA0 .2 option
+       TA0CCTL2 = OUTMOD_3 ; // CCR2 set / reset
+       TA0CTL = TASSEL_2 + MC_1 ; // SMCLK ; MC_1 -> up mode ;
+
+       while(1)
+       {
+
+       }
 	
 	return 0;
 }
